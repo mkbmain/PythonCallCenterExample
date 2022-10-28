6b60ff4990812f061ed090e5f6f05264b66f06a0
mkbmain <michael.bourke@live.co.uk>
2022-10-28 16:56:08 +0100

    comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from SqlModels import callcenter_session as session, CommunicationLog, CommunicationType, Agent, Lead, Extension
 
 
-def start_point(name):
+def start_point():
     lead_query = session.query(Lead).filter(Lead.agent_id != None)  # query
     lead = lead_query.first()  # will just get the first one but this will execute the query
     ext = session.query(Extension).first()
@@ -14,16 +14,17 @@ def start_point(name):
         join(Agent). \
         join(CommunicationType). \
         filter(CommunicationType.name == 'HomePhone'). \
-        filter(Agent.last_name == 'Smith').first()
+        filter(Agent.last_name == 'Smith').\
+        first()
 
     # with this record we can then see the extension the agent used to make that call from CommunicationLog
     ext = cl.agent.extension.extension
     print(
-        F"{cl.lead.first_name} {cl.lead.last_name} was called on there home phone {cl.lead.home_phone} on {cl.started_at}" +
-        F"\nby {cl.agent.first_name} {cl.agent.last_name} on extension {ext}")
+        F"{cl.lead.first_name} {cl.lead.last_name} was called on there home phone {cl.lead.home_phone} on {cl.started_at}"
+        + F"\nby {cl.agent.first_name} {cl.agent.last_name} on extension {ext}")
 
     result = session.query(Agent).filter(Agent.extension_id == 39)  # builds a query
-    for row in result:  ## will iterate through a open connection
+    for row in result:  # will iterate through a open connection
         print("ID:", row.id, F"Name: {row.first_name} - {row.last_name}", "extensionid:", row.extension_id)
         removed = session.query(Lead).filter(
             Lead.agent_id == row.id and Lead.convertedat != None)  # a query with in a query with lot better filters
@@ -33,4 +34,4 @@ def start_point(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    start_point('PyCharm')
+    start_point()
